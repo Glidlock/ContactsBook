82c67c27eb9b5045a17d1be1cd6fa8ee91e35005
Glidlock <howigen@gmail.com>
2023-11-02 19:48:34 +0200

    Controls & GroupsList

--- ContactsBook/ContactsBook.cpp
+++ ContactsBook/ContactsBook.cpp
@@ -5,6 +5,12 @@
 #include "ContactsBook.h"
 
 #define MAX_LOADSTRING 100
+//* Дескриптори елементів управління:
+HWND hList1, hList2;
+HWND hEdit1, hEdit2, hEdit3, hEdit4;
+HWND hButton1, hButton2, hButton3, hButton4;
+
+
 
 // Глобальные переменные:
 HINSTANCE hInst;                                // текущий экземпляр
@@ -152,6 +158,29 @@ INT_PTR CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
     switch (message)
     {
     case WM_INITDIALOG:
+        {
+           // 1 - Прив'язка дискрипторів:
+            hList1 = GetDlgItem(hDlg, IDC_LIST1);
+            hList2 = GetDlgItem(hDlg, IDC_LIST2);
+
+            hEdit1 = GetDlgItem(hDlg, IDC_EDIT1);
+            hEdit2 = GetDlgItem(hDlg, IDC_EDIT2);
+            hEdit3 = GetDlgItem(hDlg, IDC_EDIT3);
+            hEdit4 = GetDlgItem(hDlg, IDC_EDIT4);
+
+            hButton1 = GetDlgItem(hDlg, IDC_BUTTON1);
+            hButton2 = GetDlgItem(hDlg, IDC_BUTTON2);
+            hButton3 = GetDlgItem(hDlg, IDC_BUTTON3);
+            hButton4 = GetDlgItem(hDlg, IDC_BUTTON4);
+
+            // 2 - Заповнення списку груп контактів
+
+            SendMessage(hList1, LB_ADDSTRING, 0, LPARAM(L"Робочі контакти"));
+            SendMessage(hList1, LB_ADDSTRING, 0, LPARAM(L"Сімейні контакти"));
+            SendMessage(hList1, LB_ADDSTRING, 0, LPARAM(L"Контакти сусідів"));
+            SendMessage(hList1, LB_ADDSTRING, 0, LPARAM(L"Контакти знайомих"));
+            SendMessage(hList1, LB_ADDSTRING, 0, LPARAM(L"Особисті контакти"));
+        }
         return (INT_PTR)TRUE;
 
     case WM_COMMAND:
